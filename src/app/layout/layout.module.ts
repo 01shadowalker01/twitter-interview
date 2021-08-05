ba17c67b7fb3e00006ea861a81034c081d701fd7
shadowalker <01shadowalker01@gmail.com>
2021-08-05 17:15:43 +0430

    Implement header & main container, Add pictures

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,6 +4,7 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponent } from "./components/header/header.component";
@@ -17,6 +18,7 @@ import { MainContainerComponent } from "./components/main-container/main-contain
     MatListModule,
     MatIconModule,
     MatButtonModule,
+    MatToolbarModule
   ],
   exports: [MainContainerComponent],
 })
